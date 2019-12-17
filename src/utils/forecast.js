720d18939d1e493d677b30a9c44b1461d097c5c1
Philippe Robinet <ph.robinet@gmail.com>
2019-12-17 11:01:27 +0100

    update request api

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = `https://api.darksky.net/forecast/849d8b0f5f24bdef7119c48004fa9bb5/${latitude},${longitude}?units=si`;
+    const url = `https://api.darksky.net/forecast/849d8b0f5f24bdef7119c48004fa9bb5/${latitude},${longitude}?units=si&lang=fr`;
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' Il fait actuellement ' + body.currently.temperature + '°. Il y a ' + body.currently.precipProbability + '% de chance de pleuvoir.')
         }
     })
 }
