0b1ddc15650b593f708bb0868fcc6a9e0d1b4aaf
Philippe Robinet <ph.robinet@gmail.com>
2020-01-14 00:50:21 +0100

    add icon

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,48 @@
-console.log('Client side javascript file is loaded!')
+console.log("Client side javascript file is loaded!");
 
-const weatherForm = document.querySelector('form')
-const search = document.querySelector('input')
-const messageOne = document.querySelector('#message-1')
-const messageTwo = document.querySelector('#message-2')
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
 
-weatherForm.addEventListener('submit', (e) => {
-    e.preventDefault()
+weatherForm.addEventListener("submit", e => {
+  e.preventDefault();
 
-    const location = search.value
+  const location = search.value;
 
-    messageOne.textContent = 'Loading...'
-    messageTwo.textContent = ''
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
+  document.getElementById("icon").src = "";
 
-    fetch(`/weather?address=${location}`).then((response) => {
-        response.json().then((data) => {
-            if (data.error) {
-                messageOne.textContent = data.error
-            } else {
-                messageOne.textContent = data.location
-                messageTwo.textContent = data.forecast
-            }
-        })
-    })
-})
+  fetch(`/weather?address=${location}`).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        console.log(data.forecast.icon);
+
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast.text;
+        // messageThree.textContent = data.forecast.icon;
+        switch (data.forecast.icon) {
+          case "rain":
+            document.getElementById("icon").src = "/img/rain.png";
+            break;
+          case "snow":
+            document.getElementById("icon").src = "/img/snow.png";
+            break;
+          case "sun":
+            document.getElementById("icon").src = "/img/sun.png";
+            break;
+          case "storm":
+            document.getElementById("icon").src = "/img/storm.png";
+            break;
+
+          default:
+            document.getElementById("icon").src = "";
+            break;
+        }
+      }
+    });
+  });
+});
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,17 +1,32 @@
-const request = require('request')
+const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
-    const url = `https://api.darksky.net/forecast/849d8b0f5f24bdef7119c48004fa9bb5/${latitude},${longitude}?units=si&lang=fr`;
+  const url = `https://api.darksky.net/forecast/849d8b0f5f24bdef7119c48004fa9bb5/${latitude},${longitude}?units=si&lang=fr`;
 
-    request({ url, json: true }, (error, { body }) => {
-        if (error) {
-            callback('Unable to connect to weather service!', undefined)
-        } else if (body.error) {
-            callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, body.daily.data[0].summary + ' Il fait actuellement ' + body.currently.temperature + '°. Il y a ' + body.currently.precipProbability + '% de chance de pleuvoir.')
-        }
-    })
-}
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      console.log(body.daily.data[0]);
 
-module.exports = forecast
+      // body.daily.data[0] ==> c'est le corp du retour avec toutes les info à l'intèrieur
+      callback(undefined, {
+        text:
+          body.daily.data[0].summary +
+          " Il fait actuellement " +
+          body.currently.temperature +
+          "°. Il y a " +
+          body.currently.precipProbability +
+          "% de chance de pleuvoir." +
+          " La température ressentie : " +
+          body.currently.apparentTemperature +
+          "°.",
+        icon: body.daily.data[0].precipType
+      });
+    }
+  });
+};
+
+module.exports = forecast;
